dadba3805f2246b740872e06e887f3be0f715b7e
Oluwalayomi Akinrinade <layomia@gmail.com>
2016-11-05 21:41:15 -0400

    Initial commit

---
+++ app/routes/api.js
@@ -0,0 +1,157 @@
+//  api.js
+//  List
+
+var User = require("../models/user");
+
+module.exports = function(app, express) {
+	
+	var apiRouter = express.Router();
+	
+	//middleware to use for all requests
+	apiRouter.use(function(req, res, next) {
+		//authenticate users here
+		
+		next();
+	});
+	
+	//root response for api
+	//accessed at http://endorse-backend-api.herokuapp.com/api/
+	apiRouter.get("/", function(req, res) {
+		res.json({ message: "Hooray! Welcome to List API!" });
+	});
+	
+	apiRouter.route("/users")
+	
+		// create users
+		.post(function(req, res) {
+			//new instance of User model
+			var user = new User();
+			
+			//set users information (which comes from request)
+			user.firstName = req.body.firstName;
+			user.lastName  = req.body.lastName;
+			user.username  = req.body.username;
+			user.email     = req.body.email;
+			user.password  = req.body.password;
+			
+			user.save(function(err) {
+			
+				if (err) {
+					return res.send(err);
+				}
+				
+				res.json({ errmsg: "Nil", message: "User created!", user_id: user.id });
+			
+			});
+		})
+	
+		.get(function(req, res) {
+			//attempt to find all users
+			User.find(function(err, users) {
+				//if error, return error
+				if (err)
+					res.send(err);
+				
+				//return all users
+				res.json(users);
+			});
+		});
+	
+	// authenticate user for login
+	// returns json containing user id
+	apiRouter.post("/authenticate_user", function(req, res) {
+		
+		// find the user
+		// select the name email and password explicitly
+		User.findOne({
+			email: req.body.email
+		}).select('email password').exec(function(err, user) {
+
+			if (err) throw err;
+
+			// no user with that email was found
+			if (!user) {
+				res.json({
+					success: false,
+					message: 'Authentication failed. User not found.'
+				});
+			} else if (user) {
+
+				// check if password matches
+				var validPassword = user.comparePassword(req.body.password);
+				if (!validPassword) {
+					res.json({
+						success: false,
+						message: 'Authentication failed. Wrong password.'
+					});
+				} else
+					res.json(user);
+			}
+
+		});
+		
+	});
+	
+	// on routes that end in /users/:user_id
+	apiRouter.route('/users/:user_id')
+
+		// get the user with this id
+		// (accessed at GET https://endorse-backend-api.herokuapp.com/api/users/:user_id)
+		.get(function(req, res) {
+			User.findById(req.params.user_id, function(err, user) {
+				if (err) 
+					res.send(err);
+
+				// return that user
+				res.json(user);
+			});
+		})
+	
+		// update the user with this id
+		// (accessed at PUT https://endorse-backend-api.herokuapp.com/api/users/:user_id)
+		.put(function(req, res) {
+		
+			// use user model to find user
+			User.findById(req.params.user_id, function(err, user) {
+		
+				if (err) 
+					res.send(err);
+
+				// update the users info only if its new
+				if (req.body.firstName) 
+					user.firstName = req.body.firstName;
+				if (req.body.lastName) 
+					user.lastName = req.body.lastName;
+				if (req.body.email) 
+					user.email = req.body.email;
+				if (req.body.username) 
+					user.username = req.body.username;
+				if (req.body.password) 
+					user.password = req.body.password;
+
+				// save the user
+				user.save(function(err) {
+					if (err)
+						res.send(err);
+					else
+						res.json({ message: 'User updated!' });	
+				});
+
+			});
+		})
+	
+		// delete the user with this id
+		// (accessed at DELETE https://endorse-backend-api.herokuapp.com/api/users/:user_id)
+		.delete(function(req, res) {
+			User.remove({
+				_id: req.params.user_id
+			}, function(err, user) {
+				if (err) 
+					return res.send(err);
+				else
+					res.json({ message: 'Successfully deleted' });
+			});
+		});
+	
+	return apiRouter;
+}
---
+++ server.js
@@ -0,0 +1,59 @@
+//  server.js
+//  Endorse
+
+//CALL PACKAGES
+var express    = require("express");
+var app        = express(); 
+var bodyParser = require("body-parser");
+var morgan     = require("morgan");
+var mongoose   = require("mongoose");
+
+//APP Configuration
+// use body parser to grab information from POST requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// configure app to handle CORS requests
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+	next();
+});
+
+// log all requests to the console
+app.use(morgan('dev'));
+
+// Database variable outside of database connection callback to reuse connection pool in app
+var db;
+
+// Connect to the database before starting the application server.
+var localDatabase = "mongodb://localhost/endorse";
+var productionDatabase = process.env.MONGODB_URI;
+var currentDatabase = (process.env.HOME == "/Users/layomiakinrinade") ? localDatabase : productionDatabase;
+mongoose.connect(currentDatabase, function(err, database) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse
+  db = database;
+  console.log("Database connection ready");
+
+  // Initialize app
+  var server = app.listen(process.env.PORT || 8080, function () {
+	var port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+
+//ROUTES
+//api routes
+var apiRoutes  = require("./app/routes/api")(app, express);
+app.use("/api", apiRoutes);
+
+//basic route for home page
+app.get("/", function(req, res) {
+	res.send("Welcome to the homepage")
+});
